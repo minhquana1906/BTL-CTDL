d2d160ccae34e0d47585014e0d3b55fe37445a87
justwannacodee <98950227+justwannacodee@users.noreply.github.com>
2023-11-18 21:30:07 +0700

    upd

--- Bai 11/Bai1.cpp
+++ Bai 11/Bai1.cpp
@@ -1,72 +1,81 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <set>
-#include <sstream>
-#include <cctype>
+#include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-
-string transform(const string &word){
+string transform(const string &word)
+{
 	string res = word;
-	while(!res.empty() && ispunct(res.back())){
+	while (!res.empty() && ispunct(res.back()))
+	{
 		res.pop_back();
 	}
-	for(char &x:res){
+	for (char &x : res)
+	{
 		x = tolower(x);
 	}
 	return res;
 }
 
-class dictionary{
-	private:
-		set<string>s;
-	public:
-		void readFile(const string& filename){
-			ifstream readFile(filename);
-			if(!readFile.is_open()) {
-				cout<<"Mo file that bai!\n";
-				return ;
-			}
-               string word;
-			while(getline(readFile,word)) {
-			   	word = transform(word);
-			   	s.insert(word);
-			}
-			// for(auto& it:s){
-			// 	cout<<it<<endl;
-			// }
-			readFile.close();   
+class dictionary
+{
+private:
+	set<string> dict;
+
+public:
+	// ham doc file
+	void readFile(const string &filename)
+	{
+		ifstream readFile(filename);
+		if (!readFile.is_open())
+		{
+			cout << "Mo file that bai!\n";
+			return;
 		}
-		 void exportFile(const string filename){
-		 	ofstream exportFile(filename);
-		 	if(!exportFile.is_open()){
-		 		cout<<"Mo file that bai!\n";
-		 		return ;
-		 	}
-				for(auto &it:s){
-					exportFile << it << endl;
-				}
-				exportFile.close();
-		 }
-		void check() {
-			string text;
-			getline(cin,text);
-			stringstream ss(text);
-			string word;
-			int count=0;
-			while(ss >> word){
-				word=transform(word);
-				if(s.find(word) == s.end()){	
-					cout<<"- "<<word<<" sai chinh ta !\n";
-					count++;
-				}
-			}
-			if(count==0){
-				cout<<"Van ban da nhap khong co tu nao sai chinh ta !\n";
+		string word;
+		while (getline(readFile, word))
+		{
+			word = transform(word);
+			dict.insert(word);
+		}
+		readFile.close();
+	}
+	// ham xuat file
+	void exportFile(const string filename)
+	{
+		ofstream exportFile(filename);
+		if (!exportFile.is_open())
+		{
+			cout << "Mo file that bai!\n";
+			return;
+		}
+		for (auto &it : dict)
+		{
+			exportFile << it << endl;
+		}
+		exportFile.close();
+	}
+	// ham kiem tra chinh ta
+	void check()
+	{
+		cout << "Nhap doan van can kiem tra: \n";
+		string text;
+		getline(cin, text);
+		stringstream ss(text);
+		string word;
+		int count = 0;
+		while (ss >> word)
+		{
+			word = transform(word);
+			if (dict.find(word) == dict.end())
+			{
+				cout << "- " << word << " sai chinh ta !\n";
+				count++;
 			}
 		}
+		if (count == 0)
+		{
+			cout << "Van ban da nhap khong co tu nao sai chinh ta !\n";
+		}
+	}
 };
 
 int main()
@@ -74,6 +83,6 @@ int main()
 	dictionary dic;
 	dic.readFile("words.txt");
 	dic.check();
-	//dic.exportFile("outputdic.txt");
+	// dic.exportFile("outputdic.txt");
 	return 0;
 }
--- Bai 31/Bai2.cpp
+++ Bai 31/Bai2.cpp
@@ -1,248 +1,311 @@
-#include<iostream>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-// using ll = long long;
-
-//tao class Node
-class Node{
+class node
+{
 private:
-	int  value;
-	Node* left;
-	Node* right;
-    Node* parent;
+    node *parent, *left, *right;
+    int value;
+
 public:
-    //constructor:
-   Node() {
-      left = nullptr;
-      right = nullptr;
-      parent = nullptr;
-	};
-    
-	Node(int value) {
-		left = nullptr;
-		right = nullptr;
-        parent = nullptr;
-   //this la de phan biet 2 cai value :
-		this->value = value;
-	}
-    //getter : lay gia tri tu cac thuoc tinh private:
-    int getValue() {
-        return value;
+    node()
+    {
+        parent = left = right = nullptr;
+        value = 0;
     }
-    Node *getParent() {	
-        return parent;
+    node(int value)
+    {
+        parent = left = right = nullptr;
+        this->value = value;
     }
-	Node *getLeft() {
+    int getValue()
+    {
+        return this->value;
+    }
+    node *getLeft()
+    {
         return left;
     }
-	Node *getRight() { 
+    node *getRight()
+    {
         return right;
-    }   
-    //setter : dinh danh cai gia tri ma m nhap vao 
-    void setValue(int value) {
-        this->value = value;
-    }   
-    void setParent(Node *newNode) {
-         parent = newNode;
     }
-	void setLeft(Node *newNode) { 
-            left = newNode;
+    node *getParent()
+    {
+        return parent;
     }
-	void setRight(Node *newNode) { 
-            right = newNode; 
+    void setParent(node *targetNode)
+    {
+        parent = targetNode;
     }
-
-    bool hasLeft(Node* node){
-		return node!=nullptr && node->left !=nullptr;
-	}
-	bool hasRight(Node* node){
-		return node!=nullptr && node->right != nullptr;
-	}
+    void setLeft(node *targetNode)
+    {
+        left = targetNode;
+    }
+    void setRight(node *targetNode)
+    {
+        right = targetNode;
+    }
+    bool hasLeft()
+    {
+        return left != nullptr;
+    }
+    bool hasRight()
+    {
+        return right != nullptr;
+    }
 };
 
-
-class BinaryTree{
+class BinaryTree
+{
 private:
-	Node* root ;
+    node *root;
+    int sz;
+
 public:
-   //getter : 
-   Node* getRoot() { 
-    return root;
-   }
-   //constructor:
-	BinaryTree(){
-		root = nullptr;
-	}
-	Node* insert(Node* parent , int value) { 
-        Node* newNode = new Node(value);
+    BinaryTree()
+    {
+        sz = 0;
+        root = nullptr;
+    }
+    node *getRoot()
+    {
+        return root;
+    }
+
+    bool isEmpty()
+    {
+        return sz == 0;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool isRoot(node *targetNode)
+    {
+        return targetNode->getParent() == nullptr;
+    }
+    bool isInternal(node *targetNode)
+    {
+        return (targetNode->hasLeft() || targetNode->hasRight());
+    }
+    bool isExternal(node *targetNode)
+    {
+        return (!targetNode->hasLeft() && !targetNode->hasRight());
+    }
+
+    node *insert(node *parent, int value)
+    {
+        node *newNode = new node(value);
         newNode->setParent(parent);
-        //danh sach dang rong 
-        if(parent == nullptr) { 
+        if (root == nullptr)
+        {
             root = newNode;
-            return nullptr ;
         }
-        if(!parent->hasLeft(parent)) {
-           parent->setLeft(newNode);
-        }else {
-            if(!parent->hasRight(parent))
-			   parent->setRight(newNode);
-			else{
-                return nullptr;
-            } 
+        else if (!parent->hasLeft())
+        {
+            parent->setLeft(newNode);
         }
+        else if (!parent->hasRight())
+        {
+            parent->setRight(newNode);
+        }
+        else
+        {
+            return nullptr;
+        }
+        sz++;
         return newNode;
     }
 
-	//duyet tien tu
-    void preOrder(Node* node) {
-		if(node == NULL) return;	//C2:neu node hien tai = null thi dung 
-		
-		cout<< node->getValue()<<" ";
-			
-		preOrder(node->getLeft());
-			
-		preOrder(node->getRight());
-	}
-    
+    void preOrder(node *targetNode)
+    {
+        if (targetNode != nullptr)
+        {
+            cout << targetNode->getValue() << " ";
+            preOrder(targetNode->getLeft());
+            preOrder(targetNode->getRight());
+        }
+    }
+    void inOrder(node *currentNode)
+    {
+        if (currentNode == nullptr)
+            return;
 
-	//Liet ke tat ca duong di tu goc den la
-    void rootPath(Node* node , vector<int>& path) {
-        //danh sach rong:
-		if(node == nullptr ) {
-			return ;
-		}
-        //B1:
-		path.push_back(node->getValue());
-    
-		if(node->getLeft() == nullptr && node->getRight() ==nullptr) {
-			cout<<"Path: ";
-			for(int value : path){
-				cout<<value<<" -> ";
-			}	
-			cout<<"NULL\n";
-			}
-		else{
-            //B2
-			rootPath(node->getLeft() , path);
-            //B4
-			rootPath(node->getRight() , path);
-		}
-        //B4
-		path.pop_back();
-	}
-    
- 	//tinh tong tat ca duong di
-    void sumPath(Node* node , vector<int>& path , int& sumRoads) {
-            //danh sach rong:
-            if(node == nullptr) {
-                return ;
-            }
-            path.push_back(node->getValue());
-            if(node->getLeft() == nullptr && node->getRight() == nullptr) {
-                int sum = 0;
-                for(int value : path){
-                    sum = sum*10+ value;   
-                }
-            sumRoads+=sum;
-     			cout<<"Tong cua duong di: "<<sum<<endl;
-            }
-            else{
-                sumPath(node->getLeft() , path , sumRoads);
-						
-                sumPath(node->getRight() , path , sumRoads);
-            }
-            path.pop_back();
-
+        inOrder(currentNode->getLeft());
+        cout << currentNode->getValue() << " ";
+        inOrder(currentNode->getRight());
+    }
+    // Duyet cay theo thu tu hau tu
+    void postOrder(node *currentNode)
+    {
+        if (currentNode == nullptr)
+        {
+            return;
         }
 
-      void findPath(Node* node , vector<int>& path , int& findRoads,int a) {
-        	//danh sach rong:
-   		if(node == nullptr) {
-             return ;
-         }
-         vector<int>tmp;
-         path.push_back(node->getValue());
-         tmp.push_back(node->getValue());
-         if(node->getLeft() == nullptr && node->getRight() ==nullptr) {
-             int sum = 0;
-             for(int value : path){
-                 sum += value;                 
-             }
-         	if(sum==a){
-         		cout<<"True,ton tai duong di ";
-         		for(int tmp : path){
-						cout<<tmp<<" -> ";
-					}
-					cout<<"NULL ";
-					cout<<"co tong cua duong di = "<<sum<<endl; 
-         	}
-         	else{
-         		// cout<<"Khong ton tai duong di = "<<a<<endl;
-         	}
-         }
-         else{
-             findPath(node->getLeft() , path , findRoads , a);					
-             findPath(node->getRight() , path , findRoads , a);
-         }
-         path.pop_back();     
-     	}
+        postOrder(currentNode->getLeft());
+        postOrder(currentNode->getRight());
+        cout << currentNode->getValue() << " ";
+    }
 
-        //ham xoa node va cac con cua node do
-      void remove(Node* v){
-      	if(v!=nullptr){
-        		remove(v->getLeft());
-       		remove(v->getRight());
-       		delete v;
-       	}
-     	}
+    // Liet ke tat ca duong di tu goc den la
+    void PathsList(node *node, vector<int> paths)
+    {
+        // da
+        if (node == nullptr)
+        {
+            return;
+        }
+        // them cac node da duyet duoc vao trong vector
+        paths.push_back(node->getValue());
+        // neu da duyet dc 1 duong di tu goc den la =>dung lai va thuc hien task trong ngan xep
+        if (!node->hasLeft() && !node->hasRight())
+        {
+            cout << "Path: ";
+            for (int value : paths)
+            {
+                cout << value << " -> ";
+            }
+            cout << "NULL\n";
+        }
+        // neu chua duyet het duong
+        else
+        {
+            PathsList(node->getLeft(), paths);
+            PathsList(node->getRight(), paths);
+        }
+    }
+
+    // tinh tong cac so tu goc den la
+    void SumOfPath(node *node, vector<int> path, int sum)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+        path.push_back(node->getValue());
+        if (!node->getLeft() && !node->getRight())
+        {
+            cout << "Sum of path = ";
+            for (int value : path)
+            {
+                sum = sum * 10 + value;
+            }
+            cout << sum << endl;
+        }
+        else
+        {
+            SumOfPath(node->getLeft(), path, sum);
+            SumOfPath(node->getRight(), path, sum);
+        }
+    }
+
+    // cho a, xac dinh xem duong di tu goc den la nao co tong = a
+    void FindPathEqualToA(node *node, vector<int> path, int sum, int &a, int &cnt)
+    {
+        // neu la la thi return
+        if (node == nullptr)
+        {
+            return;
+        }
+        path.push_back(node->getValue());
+        if (!node->hasLeft() && !node->hasRight())
+        {
+            for (int value : path)
+            {
+                sum += value;
+            }
+            if (sum == a)
+            {
+                cout << "True, ton tai duong di ";
+                for (int value : path)
+                {
+                    cout << value << " -> ";
+                }
+                cout << "NULL co tong duong di = " << sum << endl;
+                cnt++;
+            }
+        }
+
+        else
+        {
+            FindPathEqualToA(node->getLeft(), path, sum, a, cnt);
+            FindPathEqualToA(node->getRight(), path, sum, a, cnt);
+        }
+    }
 };
 
+int main()
+{
+    BinaryTree tree;
+    vector<int> path;
+    int sum = 0;
 
-int main(){
-	BinaryTree  tree;
-	BinaryTree 	findTree;
-   vector<int> path;
-   int sumRoads = 0;
+    system("cls");
+    cout << "Size ban dau cua cay: " << tree.size() << endl;
+    cout << "cay co rong khong? ";
+    if (tree.isEmpty())
+    {
+        cout << "YES!\n";
+    }
+    else
+        cout << "NO!\n";
 
-// nhap vao cay nhi phan thu 1
-	tree.insert(tree.getRoot(),0);
-	Node* l = tree.insert(tree.getRoot(),1);
-	Node* r = tree.insert(tree.getRoot(),2);
-	Node* lL = tree.insert(l,3);
-	Node* lR = tree.insert(l,4);
-	Node* rL = tree.insert(r,5);
-	Node* rR = tree.insert(r,6);
-	tree.insert(lL,7);
-	tree.insert(lL,8);
-	tree.insert(lR,9); 
-// Nhap vao cay nhi phan thu 2
-	findTree.insert(findTree.getRoot(),5);
-	Node* l1 = findTree.insert(findTree.getRoot(),4);
-	Node* r1 = findTree.insert(findTree.getRoot(),8);
-	Node* lL1 = findTree.insert(l1,11);
-	Node* rL1 = findTree.insert(r1,13);
-	Node* rR1 = findTree.insert(r1,4);
-	findTree.insert(lL1,2);
-	findTree.insert(rL1,7);
-	findTree.insert(rR1,1); 
+    node *root = tree.insert(tree.getRoot(), 0);
+    node *node1 = tree.insert(root, 1);
+    node *node2 = tree.insert(root, 2);
+    node *node3 = tree.insert(node1, 3);
+    node *node4 = tree.insert(node1, 4);
+    node *node5 = tree.insert(node2, 5);
+    node *node6 = tree.insert(node2, 6);
+    node *node7 = tree.insert(node3, 7);
+    node *node8 = tree.insert(node3, 8);
+    node *node9 = tree.insert(node4, 9);
 
-	cout<< "==========================================\n";
-    cout<<"Duyet cay nhi phan theo tien tu: \n";
-	tree.preOrder(tree.getRoot());
-    cout << endl;
-	cout<< "==========================================\n";
-	cout<<"Danh sach duong di cua cay tu goc den la: \n";
-    tree.rootPath(tree.getRoot(),path);
-	cout<< "==========================================\n";
-    cout<<"Tong cac duong di cua cay nhi phan: \n";
-	tree.sumPath(tree.getRoot(),path,sumRoads);
-	cout<<"Tong tat ca duong di: "<<sumRoads<<endl;
-	cout<< "==========================================\n";
-    cout<<"Tim duong co tong do dai tu goc -> la = a: \n";
-    int a;
-    cout<<"Nhap vao gia tri a: ";
-    cin>>a;
-    findTree.findPath(findTree.getRoot(),path,sumRoads,a);
+    cout << "\n=====================================\n";
+    cout << "Sau khi them node: \n";
+    cout << "Size ban dau cua cay: " << tree.size() << endl;
+    cout << "cay co rong khong? ";
+    if (tree.isEmpty())
+    {
+        cout << "YES!\n";
+    }
+    else
+        cout << "NO!\n";
+
+    // duyet cay tien tu
+    cout << "\n=====================================\n";
+    cout << "duyet tien tu: ";
+    tree.preOrder(tree.getRoot());
+    cout << "\n=====================================\n";
+    // duyet trung tu
+    cout << "duyet trung tu: ";
+    tree.inOrder(tree.getRoot());
+    cout << "\n=====================================\n";
+    // duyet hau tu
+    cout << "duyet hau tu: ";
+    tree.postOrder(tree.getRoot());
+    cout << "\n=====================================\n";
+
+    // in ra tat ca duong di tu goc den la
+    cout << "\n=====================================\n";
+    cout << "TAT CA DUONG DI TU GOC DEN LA: \n";
+    tree.PathsList(tree.getRoot(), path);
+    cout << "\n=====================================\n";
+    // in ra tong duong di tu goc den la
+    cout << "TONG DUONG DI TU GOC DEN LA:  \n";
+    tree.SumOfPath(tree.getRoot(), path, sum);
+    cout << "\n=====================================\n";
+    // Tim tong cac so tu goc den la co gia tri = a
+    cout << "Nhap vao gia tri a: ";
+    int a, cnt = 0;
+    cin >> a;
+    cout << "DUONG DI TU GOC -> LA CO TONG BANG " << a << " LA: \n";
+    tree.FindPathEqualToA(tree.getRoot(), path, sum, a, cnt);
+    if (cnt == 0)
+    {
+        cout << "False, khong ton tai duong di nao co tong = " << a << " !" << endl;
+    }
     return 0;
 }
